066d4491b812f7f1a2edaec3083a90762a35cac7
fuzhou.chen <fuzhouch@gmail.com>
2015-09-21 13:46:59 +0800

    [Fix] Fix installation script to make it work on clean machines.

--- setup.cmd
+++ setup.cmd
@@ -3,7 +3,10 @@ set CheckoutDir=%~dp0
 git submodule init
 git submodule update
 if exist "%UserProfile%\vimfiles" ( rmdir "%UserProfile%\vimfiles" )
+if exist "%UserProfile%\.vim" ( rmdir "%UserProfile%\.vim" )
 if exist "%UserProfile%\.vimrc" ( del /f "%UserProfile%\.vimrc" )
 mklink /J "%UserProfile%\vimfiles" "%CheckoutDir%vimfiles"
+mklink /J "%UserProfile%\.vim" "%CheckoutDir%vimfiles"
 mklink /H "%UserProfile%\.vimrc" "%CheckoutDir%vimfiles\vimrc"
+mklink /H "%UserProfile%\vimfiles\autoload\plug.vim" "%UserProfile%\vimfiles\bundle\vim-plug\plug.vim"
 gvim -c ":PlugInstall|qa!"
--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ git submodule update
 cd $HOME
 ln -f -s "$CURRENT_PATH/vimfiles/vimrc" .vimrc
 ln -f -s "$CURRENT_PATH/vimfiles" .vim
+ln -f -s "$HOME/.vim/bundle/vim-plug/plug.vim" "$HOME/.vim/autoload/plug.vim"
 vim -c ":PlugInstall|qa!"
