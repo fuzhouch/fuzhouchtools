e2c752d05f52a511096cf90be5cb9b1362835296
fuzhou.chen <fuzhouch@gmail.com>
2018-06-03 00:38:46 +0800

    [New] Support nvim on Mac (supposed work on Linux too).

--- setup.sh
+++ setup.sh
@@ -3,9 +3,32 @@
 CURRENT_PATH=`pwd`
 git submodule init
 git submodule update
-cd $HOME
-ln -f -s "$CURRENT_PATH/vimfiles/vimrc" .vimrc
-ln -f -s "$CURRENT_PATH/vimfiles" .vim
+
+rm -f $HOME/.vim
+rm -f $HOME/.vimrc
+rm -f $HOME/.local/share/nvim/site
+
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.local/share/nvim
+
+ln -f -s "$CURRENT_PATH/vimfiles" $HOME/.vim
+ln -f -s "$CURRENT_PATH/vimfiles" $HOME/.local/share/nvim/site
+
+ln -f -s "$CURRENT_PATH/vimfiles/vimrc" $HOME/.vimrc
+ln -f -s "$CURRENT_PATH/vimfiles/vimrc" "$HOME/.config/nvim/init.vim"
+
 ln -f -s "$HOME/.vim/bundle/vim-plug/plug.vim" "$HOME/.vim/autoload/plug.vim"
-ln -f -s "$CURRENT_PATH/ctags.cnf" .ctags
-vim -c ":PlugInstall|qa!"
+
+
+VIM_INSTALLED=`which vim`
+NVIM_INSTALLED=`which nvim`
+
+if [ ! "$VIM_INSTALLED" = "" ]; then
+    vim -c ":PlugInstall|qa!"
+fi
+
+if [ ! "$NVIM_INSTALLED" = "" ]; then
+    nvim -c ":PlugInstall|qa!"
+fi
+
+ln -f -s "$CURRENT_PATH/ctags.cnf" $HOME/.ctags
