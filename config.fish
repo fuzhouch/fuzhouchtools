6b62beed6ed681bbb7798f974149cc0a9cbdfbe5
fuzhou.chen <fuzhouch@gmail.com>
2021-05-17 00:16:47 +0800

    [NEW] Set theme to Tomorrow Night for better readability.

--- config.fish
+++ config.fish
@@ -30,18 +30,34 @@ set --export EDITOR vim
 
 # For K8S use - Use Aliyun's image instead of global one to avoid being
 # blocked in China.
-function startminikube
-    minikube start --registry-mirror=https://registry.docker-cn.com
-end
+alias smk "minikube start --registry-mirror=https://registry.docker-cn.com"
+alias rm "rm -i"
+alias cp "cp -i"
+alias rm "rm -i"
 
-function rm
-    command rm -i $argv
-end
-
-function cp
-    command cp -i $argv
-end
-
-function mv
-    command mv -i $argv
-end
+# Set color scheme to Tomorrow Night
+set -U fish_color_normal normal
+set -U fish_color_command 99cc99
+set -U fish_color_quote ffcc66
+set -U fish_color_end cc99cc
+set -U fish_color_error f2777a
+set -U fish_color_match 6699cc
+set -U fish_color_redirection d3d0c8
+set -U fish_color_param d3d0c8
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 6699cc
+set -U fish_color_escape 66cccc
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 747369
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_comment ffcc66
