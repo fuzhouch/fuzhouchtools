867d969414298bbede32cf1e9404b6ce3c323287
fuzhou.chen <fuzhouch@gmail.com>
2022-03-12 12:47:14 +0800

    [FIX] Missing folder creation commands are added.

--- setup.sh
+++ setup.sh
@@ -8,9 +8,11 @@ rm -f $HOME/.vim
 rm -f $HOME/.vimrc
 rm -f $HOME/.local/share/nvim/site
 
+mkdir -p $HOME/.vim/autoload
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/nvim/syntax
 mkdir -p $HOME/.local/share/nvim
+mkdir -p $HOME/.config/vis
 
 ln -f -s "$CURRENT_PATH/vimfiles" $HOME/.vim
 ln -f -s "$CURRENT_PATH/vimfiles" $HOME/.local/share/nvim/site
@@ -40,3 +42,6 @@ ln -f -s "$CURRENT_PATH/ctags.cnf" $HOME/.ctags
 ln -f -s "$CURRENT_PATH/config.fish" "$HOME/.config/fish/config.fish"
 ln -f -s "$CURRENT_PATH/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 ln -f -s "$CURRENT_PATH/fonts.conf" "$HOME/.config/fontconfig/fonts.conf"
+
+# For vis configuration
+ln -f -s "$CURRENT_PATH/visrc.lua" "$HOME/.config/vis/visrc.lua"
