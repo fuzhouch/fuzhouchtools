77731e6a5ec77398f040c06f53aec6a5c142ddee
fuzhou.chen <fuzhouch@gmail.com>
2022-05-23 00:15:28 +0800

    [NEW] Add glow to my toolbox.

--- tools.sh
+++ tools.sh
@@ -3,6 +3,7 @@
 # Presentation tools
 # go install golang.org/x/tools/cmd/present@latest
 go install github.com/maaslalani/slides@latest
+go install github.com/charmbracelet/glow@latest 
 go install -tags 'sqlserver mysql postgres presto sqlite3' github.com/xo/usql@latest
 
 # Python tools
