2c9f31e82971e7dff3a58bed949ecb8bbb1da9f4
fuzhou.chen <fuzhouch@gmail.com>
2025-08-02 14:16:37 +0800

    [NEW] Add mermaid tool for command line.

--- tools.sh
+++ tools.sh
@@ -4,8 +4,16 @@
 # go install golang.org/x/tools/cmd/present@latest
 go install github.com/maaslalani/slides@latest
 go install github.com/charmbracelet/glow@latest 
+
+# Graphic tools
+go install github.com/goldmark/mermaid@latest
+
+# SQL tools
 go install -tags 'sqlserver mysql postgres presto sqlite3' github.com/xo/usql@latest
 
+# Zig compiler manager
+go install github.com/tristanisham/zvm@latest
+
 # Python tools
-pip install jupyter ipython bokeh pandas arrow docutils
-awsv2 --install
+# pip install jupyter ipython bokeh pandas arrow docutils
+# awsv2 --install
